589e156fc6d905f26a3ee08b41b88fa927092314
Jakub Walkowiak <minilakeyt@gmail.com>
2023-04-06 14:21:57 +0200

    basic

---
+++ src/App.js
@@ -0,0 +1,157 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Auth } from "./components/auth";
+import { db, auth, storage } from "./config/firebase";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+
+function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  // New Movie States
+  const [newMovieTitle, setNewMovieTitle] = useState("");
+  const [newReleaseDate, setNewReleaseDate] = useState(0);
+  const [isNewMovieOscar, setIsNewMovieOscar] = useState(false);
+
+  // Update Title State
+  const [updatedTitle, setUpdatedTitle] = useState("");
+
+  // File Upload State
+  const [fileUpload, setFileUpload] = useState(null);
+
+  //file Read State
+
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const moviesCollectionRef = collection(db, "movies");
+
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setMovieList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const getPhotosList = async () => {
+    setImageUrls([])
+    const imagesListRef = ref(storage, "projectFiles/");
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  };
+  useEffect(() => {
+    getMovieList();
+    getPhotosList();
+  }, []);
+
+  const onSubmitMovie = async () => {
+    try {
+      await addDoc(moviesCollectionRef, {
+        title: newMovieTitle,
+        releaseDate: newReleaseDate,
+        receivedAnOscar: isNewMovieOscar,
+        userId: auth?.currentUser?.uid,
+      });
+      getMovieList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const deleteMovie = async (id) => {
+    const movieDoc = doc(db, "movies", id);
+    await deleteDoc(movieDoc);
+  };
+
+  const updateMovieTitle = async (id) => {
+    const movieDoc = doc(db, "movies", id);
+    await updateDoc(movieDoc, { title: updatedTitle });
+  };
+
+  const uploadFile = async () => {
+    if (!fileUpload) return;
+    const filesFolderRef = ref(storage, `projectFiles/${fileUpload.name}`);
+    try {
+      await uploadBytes(filesFolderRef, fileUpload);
+      getPhotosList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Auth />
+      <hr></hr>
+
+      <div>
+        <input
+          placeholder="Movie title..."
+          onChange={(e) => setNewMovieTitle(e.target.value)}
+        />
+        <input
+          placeholder="Release Date..."
+          type="number"
+          onChange={(e) => setNewReleaseDate(Number(e.target.value))}
+        />
+        <input
+          type="checkbox"
+          checked={isNewMovieOscar}
+          onChange={(e) => setIsNewMovieOscar(e.target.checked)}
+        />
+        <label> Received an Oscar</label>
+        <button onClick={onSubmitMovie}> Submit Movie</button>
+      </div>
+      <hr></hr>
+      <div>
+        {movieList.map((movie) => (
+          <div>
+            <h1 style={{ color: movie.receivedAnOscar ? "green" : "red" }}>
+              {movie.title}
+            </h1>
+            <p> Date: {movie.releaseDate} </p>
+
+            <button onClick={() => deleteMovie(movie.id)}> Delete Movie</button>
+
+            <input
+              placeholder="new title..."
+              onChange={(e) => setUpdatedTitle(e.target.value)}
+            />
+            <button onClick={() => updateMovieTitle(movie.id)}>
+              {" "}
+              Update Title
+            </button>
+          </div>
+        ))}
+      </div>
+      <hr></hr>
+
+      <div>
+        <input type="file" onChange={(e) => setFileUpload(e.target.files[0])} />
+        <button onClick={uploadFile}> Upload File </button>
+      </div>
+      <hr></hr>
+      {imageUrls.map((url) => {
+        return <img src={url} style={{ width: "400px" }} />;
+      })}
+    </div>
+  );
+}
+
+export default App;
